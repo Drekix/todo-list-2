6d6451f14f95fbf2887505b6732568b9b62de5e2
Dreki <dreki1010@gmail.com>
2024-09-26 13:50:59 +0100

    position centered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./style.css"
 
-function App() {
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  //useState, newitem and onChange allows us to send and update to item form as we go
+  const [todos, setTodos] = useState([]);
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newItem, completed:
+        false },
+      ]
+    })
+
+    setNewItem("")
+  }
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodos(id) {
+    setTodos(currentTodos =>{
+      return (
+        currentTodos.filter(todo => todo.id !== id)
+      )
+    })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+  <form onSubmit={handleSubmit} className="new-item-form"> 
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input value={newItem} 
+        onChange={e => setNewItem(e.target.value)} 
+        type="text" 
+        id="item" 
+      />
     </div>
-  );
+    <button className="btn">Add</button>
+  </form>
+  <h1 className="header">Todo List</h1>
+  <ul className="list">
+    {todos.length === 0 && "no todos"}
+    {todos.map(todo => {
+      return (
+        <li key={todo.id}>
+          <label>
+          <input type="checkbox" checked={todo.completed}
+          onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+          {todo.title}
+        </label>
+        <button 
+        onClick={() => deleteTodos(todo.id)}
+        className="btn btn-danger">Delete</button>
+      </li>
+      )
+    })}
+  </ul>
+  </>
+  )
 }
-
-export default App;
